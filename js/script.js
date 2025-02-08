754deb76d7d6dd790a7f62973c99934611c3e677
Mariem Bejaoui <bejaoui.mariem@esprit.tn>
2025-02-08 19:23:07 +0100

    design terminé

--- js/script.js
+++ js/script.js
@@ -1,51 +1,72 @@
 // Sélection des éléments
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 // Toggle du menu
-menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('active');
+menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("active");
 
     // Changer l'icône en "X" lorsqu'on ouvre le menu
-    if (navbar.classList.contains('active')) {
-        menuIcon.classList.replace('bx-menu', 'bx-x');
+    if (navbar.classList.contains("active")) {
+        menuIcon.classList.replace("bx-menu", "bx-x");
     } else {
-        menuIcon.classList.replace('bx-x', 'bx-menu');
+        menuIcon.classList.replace("bx-x", "bx-menu");
     }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*---- scroll sections active link--*/
-let sections = document.querySelectorAll('section')
-let navLinks = document.querySelectorAll('header nav a ')
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a ");
 window.onscroll = () => {
-
-    sections.forEach(sec => {
+    sections.forEach((sec) => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
+        let id = sec.getAttribute("id");
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            navLinks.forEach((links) => {
+                links.classList.remove("active");
+                document
+                    .querySelector("header nav a[href*=" + id + "]")
+                    .classList.add("active");
             });
-        };
-    })
+        }
+    });
 }; /*--------------sticky navbar-----*/
-let header = document.querySelector('header');
-header.classList.toggle('sticky', window.scrollY > 100);
+let header = document.querySelector("header");
+header.classList.toggle("sticky", window.scrollY > 100);
+/*-------scroll reveal---------------
+ */
+
+ScrollReveal({
+    // reset: true,
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+
+ScrollReveal().reveal(
+    ".home-img, .services-container, .portfolio-box, .contact form", { origin: "bottom" }
+);
+
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+/*-------typed js---------------
+ */
+const typed = new Typed(".multiple-text", {
+    strings: [
+        "Software Engineer",
+        "Fullstack Developper",
+        "Frontend Developper",
+        "Backend Developer",
+    ],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
